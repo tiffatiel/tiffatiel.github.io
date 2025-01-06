c9a1414078bcd0024f025ba750377bbb76bb10db
tiffz <zhangtiff@gmail.com>
2025-01-06 15:12:13 -0800

    Add custom icons and GA support

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,19 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-PDY264TGX1", // Google Analytics / GA
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+      },
+    },
+  ],
 }
 
 export default config
